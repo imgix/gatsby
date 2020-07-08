411e872a7284af9cc27e055833decef5bfeea7e9
Frederick Fogerty <frederick.fogerty@gmail.com>
2020-07-08 14:51:08 +1200

    config: add jest, ts-jest, typescript

---
+++ jest.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  preset: "ts-jest",
+  testEnvironment: "node",
+  coveragePathIgnorePatterns: ["<rootDir>/packages/(?:.+?)/lib/"],
+  testPathIgnorePatterns: ["<rootDir>/packages/(?:.+?)/lib/"],
+};
