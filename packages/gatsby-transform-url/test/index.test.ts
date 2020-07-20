52b6fcbc639e935a24e7b48ab4d40878a71057fb
Frederick Fogerty <frederick.fogerty@gmail.com>
2020-07-20 11:13:17 +0200

    test(gatsby-transform-url): src should be returned

--- packages/gatsby-transform-url/test/index.test.ts
+++ packages/gatsby-transform-url/test/index.test.ts
@@ -1,5 +1,14 @@
+import { buildFixedImageData } from '../src';
+
 describe('gatsby-transform-url', () => {
-  test('renders correctly', () => {
-    expect(true);
+  describe('buildFixedImageData', () => {
+    test('should return an imgix src', () => {
+      const actual = buildFixedImageData('https://test.imgix.net/image.jpg', {
+        w: 10,
+        h: 10,
+      });
+
+      expect(actual.src).toMatch(/^https:\/\/test.imgix.net\/image.jpg\?/);
+    });
   });
 });
