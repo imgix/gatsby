f4b5da3b0f545f3dbf31d91dba08cd9a2ddde693
Frederick Fogerty <frederick.fogerty@gmail.com>
2020-08-06 14:14:35 +0200

    chore(gatsby-source-url): functional wrapper around imgix client constructor

---
+++ packages/gatsby-source-url/src/imgix-core-js-wrapper.ts
@@ -0,0 +1,10 @@
+import * as E from 'fp-ts/lib/Either';
+import ImgixClient from 'imgix-core-js';
+
+export const createImgixClient = (
+  options: ConstructorParameters<typeof ImgixClient>[0],
+): E.Either<Error, ImgixClient> =>
+  E.tryCatch(
+    () => new ImgixClient(options),
+    (e) => (e instanceof Error ? e : new Error('unknown error')),
+  );
