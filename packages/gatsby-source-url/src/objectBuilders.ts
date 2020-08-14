9d0aa3f2fb21d2a50dd32b49213f0d92a925ae05
Frederick Fogerty <frederick.fogerty@gmail.com>
2020-08-14 15:05:17 +0200

    chore(gatsby-source-url): add fluid object builders

---
+++ packages/gatsby-source-url/src/objectBuilders.ts
@@ -0,0 +1,56 @@
+import { FluidObject } from 'gatsby-image';
+import ImgixClient from 'imgix-core-js';
+import { ImgixFluidArgsResolved } from './publicTypes';
+export type BuildImgixFluidArgs = {
+  client: ImgixClient;
+  url: string;
+  sourceWidth: number;
+  sourceHeight: number;
+  secureUrlToken?: string;
+  args: ImgixFluidArgsResolved;
+};
+
+export const buildFluidObject = ({
+  client,
+  url,
+  sourceWidth,
+  sourceHeight,
+  secureUrlToken,
+  args,
+}: BuildImgixFluidArgs): FluidObject => {
+  const aspectRatio = sourceWidth / sourceHeight;
+  const maxWidth = args.maxWidth;
+
+  // const base64 = buildImgixLqipUrl(
+  //   url,
+  //   secureUrlToken,
+  // )({
+  //   ...args.imgixParams,
+  //   ...args.placeholderImgixParams,
+  // });
+
+  const src = client.buildURL(url, {
+    ...args.imgixParams,
+    w: maxWidth,
+    h: args.maxHeight,
+  });
+
+  const srcset = client.buildSrcSet(url, args.imgixParams);
+
+  /* TODO: handle these */
+  //  ({
+  //   aspectRatio,
+  //   maxWidth: maxWidth,
+  //   srcSetBreakpoints: args.srcSetBreakpoints,
+  // });
+
+  return {
+    // base64,
+    aspectRatio,
+    src,
+    srcWebp: src,
+    srcSet: srcset,
+    srcSetWebp: srcset,
+    sizes: '(min-width: 8192px) 8192px, 100vw',
+  };
+};
