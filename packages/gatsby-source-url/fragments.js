11ad711253d65fd5dd08fbab8786666ce35a5ad3
Frederick Fogerty <frederick.fogerty@gmail.com>
2020-08-19 17:54:48 +0200

    feat(gatsby-source-url): add another fluid fragment and todo for base64

--- packages/gatsby-source-url/fragments.js
+++ packages/gatsby-source-url/fragments.js
@@ -1,11 +1,23 @@
 import { graphql } from 'gatsby';
 
+// TODO: add base64 when available
 export const GatsbySourceImgixFluid = graphql`
   fragment GatsbySourceImgixFluid on SourceImgixFluid {
     aspectRatio
     src
-    srcSet
     srcWebp
+    srcSet
+    srcSetWebp
+    sizes
+  }
+`;
+
+export const GatsbySourceImgixFluid_noBase64 = graphql`
+  fragment GatsbySourceImgixFluid_noBase64 on SourceImgixFluid {
+    aspectRatio
+    src
+    srcWebp
+    srcSet
     srcSetWebp
     sizes
   }
