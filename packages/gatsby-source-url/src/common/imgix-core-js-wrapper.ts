fda9832174239f7e81181d91678dfc86828f5d91
Frederick Fogerty <frederick.fogerty@gmail.com>
2020-09-17 13:07:55 +0200

    refactor: move imgix core js wrapper

--- packages/gatsby-source-url/src/common/imgix-core-js-wrapper.ts
+++ packages/gatsby-source-url/src/common/imgix-core-js-wrapper.ts
@@ -8,3 +8,6 @@ export const createImgixClient = (
     () => new ImgixClient(options),
     (e) => (e instanceof Error ? e : new Error('unknown error')),
   );
+
+export type IBuildImgixUrl = ImgixClient['buildURL'];
+export type IBuildImgixSrcSet = ImgixClient['buildSrcSet'];
