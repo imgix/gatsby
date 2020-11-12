cb2be21798e5dc35f0ec27ac943403489bd75a56
Frederick Fogerty <frederick.fogerty@gmail.com>
2020-11-12 17:37:41 +0100

    test: ignore tests in packages/ for now

--- jest.config.js
+++ jest.config.js
@@ -2,5 +2,9 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   coveragePathIgnorePatterns: ['<rootDir>/packages/(?:.+?)/lib/'],
-  testPathIgnorePatterns: ['<rootDir>/packages/(?:.+?)/lib/'],
+  testPathIgnorePatterns: [
+    '<rootDir>/packages/(?:.+?)/lib/',
+    '<rootDir>/packages/dev-and-e2e-test-environment',
+    '<rootDir>/packages/',
+  ],
 };
