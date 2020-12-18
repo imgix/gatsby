1b6e14f150b29cacccdcc6d92ec10e2b0d7b35e6
Frederick Fogerty <frederick.fogerty@gmail.com>
2020-12-18 11:54:56 +0100

    test(transform-node): initial e2e tests

---
+++ test/dev-and-e2e/src/pages/e2e/gatsby-transform-node.tsx
@@ -0,0 +1,38 @@
+import { graphql } from "gatsby"
+import React from "react"
+
+const GatsbySourceUrl = ({ data }: { data: IPageData }) => {
+  return (
+    <div>
+      <img src={data.allPost.nodes[0].imgixImage.url} alt="URL Image" />
+      {/* <div style={{ width: 100 }}>
+        <Img
+          fluid={{ ...data.testImage.fluid, sizes: "100px" }}
+          alt="Fluid Image"
+        />
+      </div>
+      <Img fixed={data.testImage.fixed} alt="Fixed Image" /> */}
+    </div>
+  )
+}
+
+type IPageData = {
+  allPost: {
+    nodes: {
+      imgixImage: {
+        url: string;
+      }
+    }[]
+  }
+}
+export const query = graphql`
+  {
+    allPost {
+      nodes {
+        imageUrl
+      }
+    }
+  }
+`
+
+export default GatsbySourceUrl
