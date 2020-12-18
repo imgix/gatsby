0e24c830b1a79d8c007fdc8e2c7382fc57ee6105
Frederick Fogerty <frederick.fogerty@gmail.com>
2020-12-18 11:55:11 +0100

    test(e2e): test data

--- test/dev-and-e2e/gatsby-node.js
+++ test/dev-and-e2e/gatsby-node.js
@@ -5,3 +5,27 @@
  */
 
 // You can delete this file if you're not using it
+
+const TEST_NODE_TYPE = 'Post'
+
+exports.sourceNodes = async ({ actions, createNodeId, createContentDigest }) => {
+  const { createNode } = actions
+  // Data can come from anywhere, but for now create it manually
+  const testData = {
+    key: 123,
+    imageUrl: 'https://assets.imgix.net/amsterdam.jpg',
+  }
+  const nodeContent = JSON.stringify(testData)
+  const node = {
+    ...testData,
+    id: createNodeId(`${TEST_NODE_TYPE}-${testData.key}`),
+    parent: null,
+    children: [],
+    internal: {
+      type: TEST_NODE_TYPE,
+      content: nodeContent,
+      contentDigest: createContentDigest(testData),
+    },
+  }
+  createNode(node)
+}
