e6c826869aae0268f74fcb09c18e6f81aa9e9a19
Frederick Fogerty <frederick.fogerty@gmail.com>
2021-01-05 13:49:30 +0100

    chore: fp-ts utils

--- src/common/fpTsUtils.ts
+++ src/common/fpTsUtils.ts
@@ -2,8 +2,17 @@ import { flow } from 'fp-ts/lib/function';
 import * as O from 'fp-ts/lib/Option';
 import * as T from 'fp-ts/lib/Task';
 import * as TE from 'fp-ts/lib/TaskEither';
+
+/**
+ * Type hole to help which constructing applications.
+ * More: https://dev.to/gcanti/type-holes-in-typescript-2lck
+ */
 export declare function _<T>(): T;
 
+/**
+ * Convert a TaskEither to a Task composing an Option
+ * @param taskEither
+ */
 export const TaskOptionFromTE = <T>(
   taskEither: TE.TaskEither<any, T>,
 ): T.Task<O.Option<T>> =>
@@ -11,3 +20,11 @@ export const TaskOptionFromTE = <T>(
     () => T.of(O.none),
     flow(O.some, T.of),
   )(taskEither);
+
+/**
+ * Helper to expand any Typescript type.
+ * Usage:
+ *   1. Add to code: `type Expanded = Expand<TypeToExpand>`
+ *   2. Hover over in VSCode
+ */
+export type Expand<T> = T extends infer O ? { [K in keyof O]: O[K] } : never;
