21361b4145c8f1820315a7d42d055ff5e41aec13
Frederick Fogerty <frederick.fogerty@gmail.com>
2021-01-19 17:27:57 +0100

    config: untrack dist

--- dist/index.d.ts
+++
@@ -1,3 +0,0 @@
-export * from './modules/gatsby-source-url';
-export * from './modules/gatsby-transform-url';
-//# sourceMappingURL=index.d.ts.map
--- dist/modules/gatsby-source-url/gatsby-browser.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-/**
- * Implement Gatsby's Browser APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/browser-apis/
- */
-// You can delete this file if you're not using it
-//# sourceMappingURL=gatsby-browser.js.map
