f489c631a253b1413836b554da7e5d4482dfb1cc
Frederick Fogerty <frederick.fogerty@gmail.com>
2021-01-21 16:43:13 +0100

    chore: remove more dist files

--- dist/common/constants.d.ts
+++
@@ -1,2 +0,0 @@
-export declare const VERSION = "1.0.0-rc.7";
-//# sourceMappingURL=constants.d.ts.map
--- dist/modules/gatsby-transform-node/gatsby-node.d.ts
+++
@@ -1,4 +0,0 @@
-import { ICreateSchemaCustomizationHook } from 'gatsby';
-import { IImgixGatsbyOptions } from '../..';
-export declare const createSchemaCustomization: ICreateSchemaCustomizationHook<IImgixGatsbyOptions>;
-//# sourceMappingURL=gatsby-node.d.ts.map
