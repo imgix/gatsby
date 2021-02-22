cb3f965beb6d2c03d0b7f5c2d6a64a1ba94de045
Frederick Fogerty <frederick.fogerty@gmail.com>
2021-02-22 13:43:32 +0100

    chore: control breakpoints of gatsby-plugin-image srcset generation

--- src/modules/gatsby-source-url/createImgixGatsbyImageDataFieldConfig.ts
+++ src/modules/gatsby-source-url/createImgixGatsbyImageDataFieldConfig.ts
@@ -1,3 +1,4 @@
+import { stripIndent } from 'common-tags';
 import { Do } from 'fp-ts-contrib/lib/Do';
 import { pipe } from 'fp-ts/lib/function';
 import * as T from 'fp-ts/lib/Task';
@@ -10,7 +11,12 @@ import {
   IGatsbyImageHelperArgs,
 } from 'gatsby-plugin-image';
 import { getGatsbyImageFieldConfig } from 'gatsby-plugin-image/graphql-utils';
-import { GraphQLFieldResolver, GraphQLObjectType } from 'gatsby/graphql';
+import {
+  GraphQLFieldResolver,
+  GraphQLFloat,
+  GraphQLInt,
+  GraphQLObjectType,
+} from 'gatsby/graphql';
 import ImgixClient from 'imgix-core-js';
 import R from 'ramda';
 import { TaskOptionFromTE } from '../../common/fpTsUtils';
@@ -67,7 +73,6 @@ const resolveGatsbyImageData = <TSource>({
   rootValue,
   args,
 ): Promise<IGatsbyImageData | undefined> => {
-  console.log('args', args);
   return pipe(
     Do(TE.taskEither)
       .sequenceSL(() => ({
@@ -99,7 +104,14 @@ const resolveGatsbyImageData = <TSource>({
           filename: url,
           sourceMetadata: { width, height, format: 'auto' },
           // placeholderURL: await getBase64Image({ baseUrl }),
-          // TODO: implement breakpoints
+          // TODO: not a public API, need to replace with public API when implemented
+          breakpoints:
+            args.breakpoints ??
+            (imgixClient as any)._generateTargetWidths(
+              args.widthTolerance,
+              args.srcSetMinWidth,
+              args.srcSetMaxWidth,
+            ),
           // breakpoints
           formats: ['auto'],
           generateImageSource: generateImageSource(imgixClient),
@@ -146,6 +158,28 @@ export const createImgixGatsbyImageFieldConfig = <TSource>({
         ],
         defaultConfig.args,
       ),
+      widthTolerance: {
+        type: GraphQLFloat,
+        description: stripIndent`
+          This argument affects the breakpoints used for the srcsets, dictates the maximum tolerated size difference between an image's downloaded size and its rendered size. For example: setting this value to 0.1 means that an image will not render more than 10% larger or smaller than its native size. In practice, the image URLs generated for a width-based srcset attribute will grow by twice this rate. A lower tolerance means images will render closer to their native size (thereby increasing perceived image quality), but a large srcset list will be generated and consequently users may experience lower rates of cache-hit for pre-rendered images on your site.
+
+          By default this rate is set to 8 percent, which we consider to be the ideal rate for maximizing cache hits without sacrificing visual quality. Users can specify their own width tolerance by providing a positive scalar value as widthTolerance to the third options object:`,
+        defaultValue: 0.08,
+      },
+      srcSetMinWidth: {
+        type: GraphQLInt,
+        description: stripIndent`
+          This argument determines the minimum srcset width that is generated. The default is 100px.
+        `,
+        defaultValue: 100,
+      },
+      srcSetMaxWidth: {
+        type: GraphQLInt,
+        description: stripIndent`
+          This argument determines the maximum srcset width that is generated, if the layout type is FULL_WIDTH. If the layout type is CONSTRAINED, the width argument will be used. Furthermore, in every case, the maximum srcset width is constrained by the width of the source image. The default is 8192px, which is the render limit of the imgix service.
+        `,
+        defaultValue: 8192,
+      },
       // TODO: add placeholder
     },
   };
