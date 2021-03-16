e3f86c2cadb871fc901264c253d29f0e58f6096b
Frederick Fogerty <frederick.fogerty@gmail.com>
2021-03-16 15:48:33 +0100

    test(hook): both fixed and constrained images

--- test/dev-and-e2e/cypress/e2e/gatsby-transform-url.js
+++ test/dev-and-e2e/cypress/e2e/gatsby-transform-url.js
@@ -21,12 +21,26 @@ describe("gatsby-transform-url", () => {
       })
   })
 
-  it("gatsby-plugin-image image loads successfully", () => {
-    cy.findByAltText("Gatsby-plugin-image image")
-      .should("be.visible")
-      .and($img => {
-        // "naturalWidth" and "naturalHeight" are set when the image loads
-        expect($img[0].naturalWidth).to.be.greaterThan(0)
-      })
+  describe("gatsby-plugin-image hook", () => {
+    it("fixed image loads successfully", () => {
+      cy.findByAltText("Gatsby-plugin-image Fixed Image")
+        .should("be.visible")
+        .and($img => {
+          // "naturalWidth" and "naturalHeight" are set when the image loads
+          expect($img[0].naturalWidth).to.be.greaterThan(0)
+        })
+      // Check srcsets are correct width.
+      // TODO: move to unit tests
+      // .invoke("attr", "srcset")
+      // .should("match", /^.* 1x, .* 2x, .* 3x, .* 4x, .* 5x$/i)
+    })
+    it("constrained image loads successfully", () => {
+      cy.findByAltText("Gatsby-plugin-image Constrained Image")
+        .should("be.visible")
+        .and($img => {
+          // "naturalWidth" and "naturalHeight" are set when the image loads
+          expect($img[0].naturalWidth).to.be.greaterThan(0)
+        })
+    })
   })
 })
