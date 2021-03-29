e849f2a117977da8c9b75ee2dc10cdb2ef64fcc8
Frederick Fogerty <frederick.fogerty@gmail.com>
2021-03-29 15:37:49 +0200

    chore: export plugin helpers

---
+++ src/pluginHelpers.ts
@@ -0,0 +1,4 @@
+export { createImgixFixedSchemaFieldConfig } from './modules/gatsby-source-url/createImgixFixedFieldConfig';
+export { createImgixFluidSchemaFieldConfig } from './modules/gatsby-source-url/createImgixFluidFieldConfig';
+export { createImgixGatsbyImageSchemaFieldConfig } from './modules/gatsby-source-url/createImgixGatsbyImageDataFieldConfig';
+export { createImgixUrlSchemaFieldConfig } from './modules/gatsby-source-url/createImgixUrlFieldConfig';
