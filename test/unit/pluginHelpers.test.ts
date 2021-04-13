5e4fd121ac363ffe1f33f561e5bf374d230dcd40
Frederick Fogerty <frederick.fogerty@gmail.com>
2021-04-13 12:53:19 +0200

    test: add some plugin helper tests

---
+++ test/unit/pluginHelpers.test.ts
@@ -0,0 +1,26 @@
+import { createImgixUrlSchemaFieldConfig } from '../../src/pluginHelpers';
+
+test('should be able to call createImgixUrlFieldConfig with no domain and resolve a url', async () => {
+  const config = createImgixUrlSchemaFieldConfig({
+    resolveUrl: (node) => (node as any).url,
+  });
+
+  const resolved = await (config as any).resolve(
+    { url: 'https://assets.imgix.net/amsterdam.jpg' },
+    {},
+  );
+
+  expect(resolved).toMatch(/^https:\/\/assets.imgix.net\/amsterdam.jpg\?/);
+});
+test('should be able to call createImgixUrlFieldConfig with a domain and resolve a url', async () => {
+  const config = createImgixUrlSchemaFieldConfig({
+    resolveUrl: (node) => (node as any).url,
+    imgixClientOptions: { domain: 'assets.imgix.net' },
+  });
+
+  const resolved = await (config as any).resolve({ url: 'amsterdam.jpg' }, {});
+
+  expect(resolved).toMatch(/^https:\/\/assets.imgix.net\/amsterdam.jpg\?/);
+});
+
+
