c767fe4ebef2672ffd6c07bd1c14e23eab0b81bc
Frederick Fogerty <frederick.fogerty@gmail.com>
2021-04-13 12:45:31 +0200

    chore: use URL for some things

--- src/common/uri.ts
+++ src/common/uri.ts
@@ -1,4 +1,22 @@
+import * as E from 'fp-ts/lib/Either';
 import Uri from 'jsuri';
-export const parseHost = (uri: string) => new Uri(uri).host();
 
-export const parsePath = (uri: string) => new Uri(uri).path();
+/**
+ * Parse the host from a URL. Can fail - for an FP type, use parseHostE
+ */
+export const parseHost = (uri: string): string => new URL(uri).hostname;
+export const parseHostE = (uri: string) =>
+  E.tryCatch(
+    () => parseHost(uri),
+    () => new Error('Invalid URL'),
+  );
+
+/**
+ * Parse the path from a URL. Can fail - for an FP type, use parsePathE
+ */
+export const parsePath = (uri: string): string => new Uri(uri).path();
+export const parsePathE = (uri: string) =>
+  E.tryCatch(
+    () => parsePath(uri),
+    () => new Error('Invalid URL'),
+  );
