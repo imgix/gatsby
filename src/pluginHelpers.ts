400a72552275c9de1948b7bd7f997e42fa37ef37
Frederick Fogerty <frederick.fogerty@gmail.com>
2021-04-26 11:51:20 +0200

    feat: helper for third-party libraries

--- src/pluginHelpers.ts
+++ src/pluginHelpers.ts
@@ -1,4 +1,22 @@
+import { createImgixURLBuilder } from './common/imgix-js-core-wrapper';
+import { buildImgixGatsbyTypes } from './modules/gatsby-source-url/typeBuilder';
+
 export { createImgixFixedSchemaFieldConfig } from './modules/gatsby-source-url/createImgixFixedFieldConfig';
 export { createImgixFluidSchemaFieldConfig } from './modules/gatsby-source-url/createImgixFluidFieldConfig';
 export { createImgixGatsbyImageSchemaFieldConfig } from './modules/gatsby-source-url/createImgixGatsbyImageDataFieldConfig';
 export { createImgixUrlSchemaFieldConfig } from './modules/gatsby-source-url/createImgixUrlFieldConfig';
+
+export const createImgixGatsbyTypes = ({
+  imgixClientOptions,
+  ...params
+}: Omit<
+  Parameters<typeof buildImgixGatsbyTypes>[0],
+  'imgixClient' | 'resolveUrl'
+> & {
+  imgixClientOptions?: Parameters<typeof createImgixURLBuilder>[0];
+  resolveUrl: (source: any) => string | null;
+}) =>
+  buildImgixGatsbyTypes({
+    ...params,
+    imgixClient: createImgixURLBuilder(imgixClientOptions),
+  });
