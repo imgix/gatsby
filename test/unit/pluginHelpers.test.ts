9f086b5132f0a127dcb43c9f51b5b4c072ee0aa9
Frederick Fogerty <frederick.fogerty@gmail.com>
2021-04-30 10:02:57 +0200

    test: resolveWidth and resolveHeight

--- test/unit/pluginHelpers.test.ts
+++ test/unit/pluginHelpers.test.ts
@@ -20,5 +20,72 @@
 
 //   expect(resolved).toMatch(/^https:\/\/assets.imgix.net\/amsterdam.jpg\?/);
 // });
+describe('plugin helpers', () => {
+  describe('resolveWidth and resolveHeight', () => {
+    it('should not make a request to imgix when resolveWidth and resolveHeight passed', async () => {
+      const fetchImgixMetadataModule = require('../../src/api/fetchImgixMetadata');
+      const spy = jest.spyOn(fetchImgixMetadataModule, 'fetchImgixMetadata');
 
-test.todo('Placeholder');
+      const { createImgixGatsbyTypes } = require('../../src/pluginHelpers');
+
+      const types = createImgixGatsbyTypes({
+        cache: mockGatsbyCache as any,
+        resolveUrl: () => 'https://assets.imgix.net/amsterdam.jpg',
+        resolveHeight: () => 200,
+        resolveWidth: () => 300,
+      });
+
+      await (types.fields.fluid.resolve &&
+        types.fields.fluid.resolve(
+          {},
+          {
+            imgixParams: {},
+          },
+          {},
+          {} as any,
+        ));
+
+      expect(spy).not.toBeCalled();
+
+      jest.resetModules();
+    });
+    it('should make a request to imgix when resolveWidth and resolveHeight not passed', async () => {
+      const fetchImgixMetadataModule = require('../../src/api/fetchImgixMetadata');
+      const spy = jest.spyOn(fetchImgixMetadataModule, 'fetchImgixMetadata');
+
+      const { createImgixGatsbyTypes } = require('../../src/pluginHelpers');
+
+      const types = createImgixGatsbyTypes({
+        cache: mockGatsbyCache as any,
+        resolveUrl: () => 'https://assets.imgix.net/amsterdam.jpg',
+      });
+
+      await (types.fields.fluid.resolve &&
+        types.fields.fluid.resolve(
+          {},
+          {
+            imgixParams: {},
+          },
+          {},
+          {} as any,
+        ));
+
+      expect(spy).toBeCalled();
+
+      jest.resetModules();
+    });
+  });
+});
+
+const mockGatsbyCache = {
+  store: {} as { [k: string]: any },
+  async get(key: string) {
+    // trace(`Getting cache value for`, log)(key);
+    return this.store[key];
+  },
+  async set(key: string, value: any) {
+    // trace(`Setting cache value for ${key} to ${value}`, log)(value);
+    this.store[key] = value;
+    return value;
+  },
+} as any;
