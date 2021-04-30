4767d4e32b499cbf86b128d05e2c50024f7bfbb7
Frederick Fogerty <frederick.fogerty@gmail.com>
2021-04-30 10:43:59 +0200

    chore: lint

--- src/modules/gatsby-source-url/createImgixGatsbyImageDataFieldConfig.ts
+++ src/modules/gatsby-source-url/createImgixGatsbyImageDataFieldConfig.ts
@@ -10,20 +10,20 @@ import {
   getLowResolutionImageURL,
   IGatsbyImageData,
   IGatsbyImageHelperArgs,
-  ImageFormat
+  ImageFormat,
 } from 'gatsby-plugin-image';
 import { getGatsbyImageFieldConfig } from 'gatsby-plugin-image/graphql-utils';
 import { GraphQLFieldResolver } from 'gatsby/graphql';
 import {
   ComposeInputTypeDefinition,
   ObjectTypeComposerArgumentConfigMapDefinition,
-  ObjectTypeComposerFieldConfigAsObjectDefinition
+  ObjectTypeComposerFieldConfigAsObjectDefinition,
 } from 'graphql-compose';
 import { TypeAsString } from 'graphql-compose/lib/TypeMapper';
 import R from 'ramda';
 import {
   fetchImgixBase64Image,
-  fetchImgixDominantColor
+  fetchImgixDominantColor,
 } from '../../api/fetchBase64Image';
 import { createExternalHelper } from '../../common/createExternalHelper';
 import { TaskOptionFromTE } from '../../common/fpTsUtils';
@@ -31,7 +31,7 @@ import { IImgixURLBuilder } from '../../common/imgix-js-core-wrapper';
 import {
   ImgixSourceDataResolver,
   resolveUrlFromSourceData,
-  taskEitherFromSourceDataResolver
+  taskEitherFromSourceDataResolver,
 } from '../../common/utils';
 import { IImgixParams, ImgixUrlParams } from '../../publicTypes';
 import { resolveDimensions } from './resolveDimensions';
@@ -60,7 +60,7 @@ const resolveGatsbyImageData = <TSource>({
   resolveWidth = () => undefined,
   resolveHeight = () => undefined,
   cache,
-  defaultParams
+  defaultParams,
 }: {
   imgixClient: IImgixURLBuilder;
   resolveUrl: ImgixSourceDataResolver<TSource, string>;
@@ -116,10 +116,9 @@ const resolveGatsbyImageData = <TSource>({
             formats: ['auto'] as ImageFormat[],
             generateImageSource: generateImageSource(imgixClient),
             options: {
-              imgixParams:
-              {
+              imgixParams: {
                 ...defaultParams,
-                ...args.imgixParams
+                ...args.imgixParams,
               },
             },
           } as const),
