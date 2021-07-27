29e0679de43ef0d811447f7363a769d0e73fa83d
Frederick Fogerty <frederick.fogerty@gmail.com>
2021-07-27 13:27:19 +0200

    feat: use @imgix/js-core for GraphQL APIs

--- src/common/imgix-js-core-wrapper.ts
+++ src/common/imgix-js-core-wrapper.ts
@@ -1,9 +1,6 @@
 import ImgixClient from '@imgix/js-core';
-import { Do } from 'fp-ts-contrib/lib/Do';
-import { sequenceS } from 'fp-ts/Apply';
 import * as E from 'fp-ts/Either';
-import { pipe } from 'fp-ts/function';
-import { parseHostE, parsePathE } from './uri';
+import { VERSION } from './constants';
 
 /**
  * An FP wrapper around new ImgixClient()
@@ -31,39 +28,48 @@ export const createImgixClient = ({
 /**
  * Used by createImgixURLBuilder, common code extracted here to avoid code duplication.
  */
-export const createURLBuilderFn = <T extends 'buildURL' | 'buildSrcSet'>(
+export const createURLBuilderFn = <T extends '_buildURL' | '_buildSrcSet'>(
   fn: T,
 ) => (options?: Parameters<typeof createImgixClient>[0]) => (
-  ...args: Parameters<InstanceType<typeof ImgixClient>[T]>
-): string =>
-  pipe(
-    Do(E.either)
-      .bindL('urlParts', () => {
-        if (options?.domain) {
-          return E.right({
-            domain: options?.domain,
-            path: args[0],
-          });
-        }
-        return sequenceS(E.either)({
-          domain: parseHostE(args[0]),
-          path: parsePathE(args[0]),
-        });
-      })
-      .bindL('client', ({ urlParts: { domain } }) =>
-        createImgixClient({
-          ixlib: 'gatsbyFP',
-          ...options,
-          domain,
-        }),
-      )
-      .return(({ client, urlParts: { path } }) =>
-        client[fn](path, ...args.slice(1)),
-      ),
-    E.getOrElse<Error, string>((err) => {
-      throw err;
-    }),
-  );
+  ...args: Parameters<typeof ImgixClient[T]>
+): string => {
+  const imgixParams = args[1];
+  const clientOptions = fn === '_buildURL' ? args[2] : (args as any)[3];
+  const url: string = (() => {
+    if (options?.domain) {
+      // Prepend host to URL string to support Web Proxy Sources
+      // When a Web Proxy Source is used, the args[0] will be a full URL,
+      // for other sources it will simply be a path
+      return `${options?.domain}/${args[0]}`;
+    }
+    return args[0];
+  })();
+
+  const mergedOptions: Parameters<typeof ImgixClient['_buildURL']>[2] = {
+    includeLibraryParam: false,
+    ...options,
+    ...clientOptions,
+    libraryParam:
+      options?.ixlib ?? clientOptions?.ixlib ?? `gatsbyFP-${VERSION}`,
+  };
+  try {
+    if (fn === '_buildURL') {
+      return ImgixClient._buildURL(url, imgixParams, mergedOptions);
+    } else {
+      const srcSetOptions = args[2];
+      return ImgixClient._buildSrcSet(
+        url,
+        imgixParams,
+        srcSetOptions,
+        mergedOptions,
+      );
+    }
+  } catch (error) {
+    throw new Error(
+      'URL construction failed. Make sure either domain is set in gatsby-config.js, or the image path has a domain included.',
+    );
+  }
+};
 
 /**
  * Build a functional ImgixClient. Allows this application to use ImgixClient in a functional manner rather than a instance/class-based manner.
@@ -72,8 +78,8 @@ export const createURLBuilderFn = <T extends 'buildURL' | 'buildSrcSet'>(
 export const createImgixURLBuilder = (
   options?: Parameters<typeof createImgixClient>[0],
 ): IImgixURLBuilder => ({
-  buildURL: createURLBuilderFn('buildURL')(options),
-  buildSrcSet: createURLBuilderFn('buildSrcSet')(options),
+  buildURL: createURLBuilderFn('_buildURL')(options),
+  buildSrcSet: createURLBuilderFn('_buildSrcSet')(options),
 });
 
 export type IBuildImgixUrl = ImgixClient['buildURL'];
