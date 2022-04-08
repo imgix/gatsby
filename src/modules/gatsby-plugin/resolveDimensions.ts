27a6fa6fe2881fdb2559f3b0a5b3bd23d84f18ec
Frederick Fogerty <frederick.fogerty@gmail.com>
2022-04-08 10:42:38 +0100

    chore: refactor body of resolveDimensions

--- src/modules/gatsby-plugin/resolveDimensions.ts
+++ src/modules/gatsby-plugin/resolveDimensions.ts
@@ -1,12 +1,10 @@
 import { sequenceS, sequenceT } from 'fp-ts/Apply';
 import * as O from 'fp-ts/Option';
-import { Option } from 'fp-ts/Option';
-import { pipe } from 'fp-ts/pipeable';
 import * as TE from 'fp-ts/TaskEither';
 import { GatsbyCache } from 'gatsby';
 import { fetchImgixMetadata } from '../../api/fetchImgixMetadata';
 import { IImgixURLBuilder } from '../../common/imgix-js-core-wrapper';
-import { createLogger, trace } from '../../common/log';
+import { createLogger, traceJSON } from '../../common/log';
 
 const sequenceTTE = sequenceT(TE.taskEither);
 const sequenceSO = sequenceS(O.option);
@@ -14,6 +12,18 @@ const sequenceSO = sequenceS(O.option);
 const log = createLogger('resolveDimensions');
 // TODO: maybe use io-ts PositiveNumber
 export type IResolveDimensionsRight = { width: number; height: number };
+
+/**
+ * Calculate the dimensions of an image, using manual dimensions, if provided,
+ * or otherwise by using the imgix API
+ * @param param0
+ * @param param0.url The URL of the image
+ * @param param0.manualWidth The width of the image, if known
+ * @param param0.manualHeight The height of the image, if known
+ * @param param0.cache Gatsby cache
+ * @param param0.client Instance of the imgix client
+ * @returns The width and height of the image, or an error
+ */
 export const resolveDimensions = <TSource>({
   url,
   manualHeight,
@@ -21,38 +31,42 @@ export const resolveDimensions = <TSource>({
   cache,
   client,
 }: {
-  manualHeight: Option<number>;
-  manualWidth: Option<number>;
+  manualHeight?: number;
+  manualWidth?: number;
   cache: GatsbyCache;
   url: string;
   client: IImgixURLBuilder;
 }): TE.TaskEither<Error, IResolveDimensionsRight> => {
-  const WidthHeightTE: TE.TaskEither<
-    Error,
-    { width: number; height: number }
-  > = pipe(
-    sequenceSO({ width: manualWidth, height: manualHeight }),
-    O.fold(
-      () => TE.left(new Error(`Couldn't find manual width on obj`)),
-      TE.right,
-    ),
-  );
+  if (manualWidth != null && manualHeight != null) {
+    traceJSON(
+      { manualWidth: manualWidth, manualHeight: manualHeight },
+      'manual dimensions used',
+      log,
+    );
+    return TE.right({
+      width: manualWidth,
+      height: manualHeight,
+    });
+  }
 
-  return pipe(
-    WidthHeightTE,
-    TE.map(trace('manual width and height', log)),
-    TE.orElse(() => {
-      return pipe(
-        TE.tryCatch(
-          () => fetchImgixMetadata(cache, client)(url),
-          () => new Error('Something went wrong fetching the image metadata'),
-        ),
-        TE.map(trace('fetchImgixMetadata result', log)),
-        TE.map(({ PixelWidth, PixelHeight }) => ({
-          width: PixelWidth,
-          height: PixelHeight,
-        })),
+  return TE.tryCatch(
+    async () => {
+      log(
+        'no manual dimensions found, fetching image dimensions from imgix API',
       );
-    }),
+
+      const { PixelWidth, PixelHeight } = await fetchImgixMetadata(
+        cache,
+        client,
+      )(url);
+
+      traceJSON({ PixelWidth, PixelHeight }, 'metadata result', log);
+
+      return {
+        width: PixelWidth,
+        height: PixelHeight,
+      };
+    },
+    () => new Error('Something went wrong fetching the image metadata'),
   );
 };
