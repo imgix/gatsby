a8b4157f73588a5dd4dd6fb9dd8ab636761e0e2f
Frederick Fogerty <frederick.fogerty@gmail.com>
2022-04-08 10:51:29 +0100

    chore: remove unneeded helpers

--- src/modules/gatsby-plugin/resolveDimensions.ts
+++ src/modules/gatsby-plugin/resolveDimensions.ts
@@ -1,16 +1,10 @@
-import { sequenceS, sequenceT } from 'fp-ts/Apply';
-import * as O from 'fp-ts/Option';
-import * as TE from 'fp-ts/TaskEither';
 import { GatsbyCache } from 'gatsby';
 import { fetchImgixMetadata } from '../../api/fetchImgixMetadata';
 import { IImgixURLBuilder } from '../../common/imgix-js-core-wrapper';
 import { createLogger, traceJSON } from '../../common/log';
 
-const sequenceTTE = sequenceT(TE.taskEither);
-const sequenceSO = sequenceS(O.option);
-
 const log = createLogger('resolveDimensions');
-// TODO: maybe use io-ts PositiveNumber
+
 export type IResolvedDimensions = { width: number; height: number };
 
 /**
