e6efa57114c52d4a1cb0a5d56b46cd600ee08319
Frederick Fogerty <frederick.fogerty@gmail.com>
2022-04-08 09:09:12 +0100

    chore: add log function and jsdoc

--- src/common/log.ts
+++ src/common/log.ts
@@ -1,9 +1,23 @@
 import debug from 'debug';
 
-const ns = 'imgix:gatsby-source-url';
+const ns = 'imgix:gatsby';
+
+/**
+ * Log a message in the @imgix/gatsby namespace
+ */
 export const log = debug(ns);
+/**
+ * Create a custom logger to group logs by a namespace.
+ * @param module The namespace to use for the logger
+ */
 export const createLogger = (module: string) => debug(`${ns}:${module}`);
 
+/**
+ * Create a tracer (function that can be used to log a message)
+ * @param label A prefix to add to the message
+ * @param logger Custom debug logger to use if needed
+ * @returns A function that takes a message to log
+ */
 export const trace = (label?: string, customLogger?: debug.IDebugger) => <T>(
   v: T,
 ) => {
@@ -11,4 +25,21 @@ export const trace = (label?: string, customLogger?: debug.IDebugger) => <T>(
     `${label ? `${label}: ` : ''}${JSON.stringify(v, null, 2)}`,
   );
   return v;
+};
+
+/**
+ * Log out a JSON value.
+ * @param rawJSON JSON data to lo
+ * @param label Prefix to add to log message
+ * @param customLogger Custom debug logger to use if created
+ */
+export const traceJSON = (
+  rawJSON: any,
+  label?: string | null | undefined,
+  customLogger?: debug.IDebugger,
+) => {
+  const logger = customLogger ?? log;
+  const prefix = label ? `${label}: ` : '';
+  const formattedMessage = JSON.stringify(rawJSON, null, 2);
+  logger(`${prefix}${formattedMessage}`);
 };
