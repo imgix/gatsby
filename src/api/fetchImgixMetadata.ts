c60d282f1b3fe9d0145c0b941415d6d1c7f07ee7
Frederick Fogerty <frederick.fogerty@gmail.com>
2022-04-08 09:20:33 +0100

    chore(fp-ts): refactor cache helpers

--- src/api/fetchImgixMetadata.ts
+++ src/api/fetchImgixMetadata.ts
@@ -1,42 +1,62 @@
-import * as E from 'fp-ts/Either';
-import { flow, pipe } from 'fp-ts/function';
 import * as TE from 'fp-ts/TaskEither';
 import { GatsbyCache } from 'gatsby';
-import * as t from 'io-ts';
 import { withCache } from '../common/cache';
 import { IImgixURLBuilder } from '../common/imgix-js-core-wrapper';
 import { createLogger, trace } from '../common/log';
-import { fetchJSON } from '../common/utils';
 
 const log = createLogger('fetchImgixMetadata');
 
-export const ImgixMetadata = t.type({
-  'Content-Type': t.string,
-  PixelWidth: t.number,
-  PixelHeight: t.number,
-});
-export type IImgixMetadata = t.TypeOf<typeof ImgixMetadata>;
+export type IImgixMetadata = {
+  'Content-Type': string;
+  PixelWidth: number;
+  PixelHeight: number;
+};
 
+/**
+ * Fetch the metadata for a given image from the imgix API
+ */
+const fetchImgixMetadataAPI = async (
+  rawURL: string,
+  client: IImgixURLBuilder,
+): Promise<IImgixMetadata> => {
+  const url = client.buildURL(rawURL, { fm: 'json' });
+  trace('imgix metadata url', log)(url);
+
+  const res = await fetch(url);
+
+  if (!res.ok) {
+    throw new Error('Something went wrong while fetching the imgix metadata');
+  }
+
+  let json;
+  try {
+    json = (await res.json()) as IImgixMetadata;
+  } catch (error) {
+    throw new Error(
+      'Something went wrong while decoding the imgix metadata: ' +
+        String(error),
+    );
+  }
+  trace('imgix metadata result', log)(json);
+
+  return json;
+};
+
+/**
+ * Fetch the metadata for a given image from the imgix API, and cache the result
+ * @param cache Gatsby cache
+ * @param client Instance of the imgix client
+ * @returns The metadata, or an error
+ */
 export const fetchImgixMetadata = (
   cache: GatsbyCache,
   client: IImgixURLBuilder,
-) => (url: string): TE.TaskEither<Error, IImgixMetadata> =>
-  withCache(`gatsby-plugin-imgix-metadata-${url}`, cache, () =>
-    pipe(
-      client.buildURL(url, { fm: 'json' }),
-      trace('imgix metadata url', log),
-      fetchJSON,
-      TE.map(trace('imgix metadata result', log)),
-      TE.chain(
-        flow(
-          ImgixMetadata.decode,
-          E.orElse(() =>
-            E.left(new Error('Problem when decoding imgix metadata.')),
-          ),
-          TE.fromEither,
-        ),
+) => (url: string): TE.TaskEither<Error, IImgixMetadata> => {
+  return TE.tryCatch(
+    () =>
+      withCache(`gatsby-plugin-imgix-metadata-${url}`, cache, () =>
+        fetchImgixMetadataAPI(url, client),
       ),
-      TE.map(trace('decoded data', log)),
-      TE.mapLeft(trace('imgix metadata error', log)),
-    ),
+    () => new Error(`Couldn't fetch imgix metadata for ${url}`),
   );
+};
